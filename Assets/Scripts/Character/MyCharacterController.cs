cbae56379f019fc6fe31034fbc53a0c755bd2b4d
Josie Thompson <dot.slash.josie@gmail.com>
2025-01-14 09:01:31 -0800

    Restructured movement script to be part of a controller hierarchy, anticipating implementing enemies

---
+++ Assets/Scripts/Character/MyCharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Character
+{
+    public class MyCharacterController : MonoBehaviour
+    {
+        [Header("Configure")]
+        public float MovementSpeed = 5; // JT: made public so that other scripts can change this in future
+
+        [Header("Debug")]
+        [SerializeField] protected Vector3 m_moveDirection; // JT: made protected so subclasses can change
+        
+        // Unity Events
+        protected virtual void FixedUpdate()
+        {
+            Move();
+        }
+
+        // Character Controller Interface
+        protected virtual void Move()
+        {
+            transform.localPosition += Time.fixedDeltaTime * MovementSpeed * m_moveDirection;
+        }
+
+        protected virtual void Attack()
+        {
+            Debug.Log("Attack!");
+        }
+    }
+}
---
+++ Assets/Scripts/Character/MyPlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Character
+{
+    public class MyPlayerController : MyCharacterController
+    {
+        // Input Actions
+        private void OnMove(InputValue value)
+        {
+            m_moveDirection = value.Get<Vector2>();
+        }
+
+        private void OnAttack(InputValue value)
+        {
+            Attack();
+        }
+    }
+}
